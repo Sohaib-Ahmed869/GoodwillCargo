eb7548ce20ed54439c4f0ba2018f5325d116a39c
Haider Ali <haideraliatish500@gmail.com>
2024-10-31 16:24:00 +0500

    adjust home page image

--- src/home/Page.jsx
+++ src/home/Page.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../component/common/Hero";
 import Services from "../component/home/Services";
 import Insides from "../component/home/Insites";
-import GlobeDelivery from "../component/common/GlobeDelivery";
 
 const Page = () => {
   const title = "From Doorstep to Destination, We've Got You Covered";
@@ -11,7 +10,7 @@ const Page = () => {
       <Hero title={title} />
       <Services />
       <Insides />
-      {/* <GlobeDelivery /> */}
+     
     </>
   );
 };
