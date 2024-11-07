fee73e9ac4d97ceb417308d152150ef704da7ec5
Haider Ali <haideraliatish500@gmail.com>
2024-11-07 10:54:09 +0500

    adding padding bottom in global delivery page

--- src/component/common/GlobeDelivery.jsx
+++ src/component/common/GlobeDelivery.jsx
@@ -3,7 +3,7 @@ import InsiteImage from "../../assets/home/insites-img.png";
 
 const GlobeDelivery = () => {
   return (
-    <div className="flex flex-col gap-5 h-full w-full  sm:gap-10 sm:px-9 my-16 items-center justify-center">
+    <div className="flex flex-col gap-5 h-full w-full  sm:gap-10 sm:px-9 mb-16 lg:mb-0 lg:mt-16 items-center justify-center">
       <h1 className="text-3xl sm:text-4xl xl:text-6xl 2xl:text-6xl font-medium text-center">
         Seamless Delivery Across the Globe.
       </h1>
